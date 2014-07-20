36a7c91e443f8cdfc1fb0ee5e701bc3e6facb953
Gowiem <gowie.matt@gmail.com>
2014-07-20 14:38:39 -0400

    Updates the MainActivity to implement the Artisan interface

--- src/com/artisan/thisishardcore/MainActivity.java
+++ src/com/artisan/thisishardcore/MainActivity.java
@@ -5,12 +5,17 @@ import org.joda.time.DateTimeZone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.artisan.activity.ArtisanActivity;
+import com.artisan.services.ArtisanBoundActivity;
+import com.artisan.services.ArtisanService;
 import com.artisan.thisishardcore.imageutils.ImageFetcher;
 import com.artisan.thisishardcore.logging.TIHConfigureLog4j;
 import com.artisan.thisishardcore.logging.TIHLogger;
@@ -20,34 +25,83 @@ import com.artisan.thisishardcore.schedule.EventListAdapter;
 import com.artisan.thisishardcore.schedule.ScheduleFragment;
 import com.artisan.thisishardcore.utils.TIHUtils;
 
-public class MainActivity extends SherlockFragmentActivity implements com.actionbarsherlock.app.ActionBar.TabListener{
+public class MainActivity extends SherlockFragmentActivity implements com.actionbarsherlock.app.ActionBar.TabListener, ArtisanBoundActivity {
 	private final TIHLogger logger = new TIHLogger(MainActivity.class);
-	
-	public static final String TAB_SELECTED = "TAB_SELECTED"; 
+
+	public static final String TAB_SELECTED = "TAB_SELECTED";
 	public final String NEWS_TAB = "NEWS_TAB";
 	public final String SCHEDULE_TAB = "SCHEDULE_TAB";
 	public final String PHOTO_PIT_TAB = "PHOTO_PIT_TAB";
-	
+
 	private NewsFragment newsFragment;
 	private ScheduleFragment scheduleFragment;
 	private PhotoPitFragment photoPitFragment;
-	
+
 	public String currentlySelectedTab;
-	 
+
+	// Artisan Interface Methods
+	/////////////////////////////
+
+	@Override
+	public ArtisanService getArtisanService() {
+		return ArtisanActivity._getArtisanService();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Call Artisan AFTER super onStart
+		ArtisanActivity.artisanOnStart(this);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		// Call Artisan AFTER super onStop
+		ArtisanActivity.artisanOnStop(this);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// Call Artisan method BEFORE super onDestroy
+		ArtisanActivity.artisanOnDestroy();
+		super.onDestroy();
+	}
+
+	@Override
+	public void setContentView(int layoutResID) {
+		View view = ArtisanActivity.artisanGetContentView(layoutResID, this);
+		super.setContentView(view);
+	}
+
+	@Override
+	public void setContentView(View view) {
+		view = ArtisanActivity.artisanGetContentView(view, this);
+		super.setContentView(view);
+	}
+
+	@Override
+    public void setContentView(View view, LayoutParams params) {
+        View contentView = ArtisanActivity.artisanGetContentView(view, params, this);
+        super.setContentView(contentView);
+    }
+
 	// Lifecycle
 	/////////////
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Call Artisan AFTER super onCreate
+		ArtisanActivity.artisanOnCreate(this);
 		setContentView(R.layout.main);
 		TIHConfigureLog4j.configure();
 		//logger.d("onCreate");
 		Thread.setDefaultUncaughtExceptionHandler(new TIHExceptionHandler(this));
-		
-		// Setting Default TimeZone for Application since I didn't know where else to put it.. It's later. 
+
+		// Setting Default TimeZone for Application since I didn't know where else to put it.. It's later.
 		DateTimeZone.setDefault(DateTimeZone.UTC);
-		
+
 		final ActionBar bar = getSupportActionBar();
 		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		bar.addTab(bar.newTab()
@@ -61,10 +115,10 @@ public class MainActivity extends SherlockFragmentActivity implements com.action
 		bar.addTab(bar.newTab()
 				.setText("Photo Pit")
 				.setTag(PHOTO_PIT_TAB)
-				.setTabListener(this));	
+				.setTabListener(this));
 
 		setupImageFetcher();
-		
+
 		//logger.d("savedInstanceState: ", savedInstanceState);
 		if (savedInstanceState != null) {
 			String tabIdentifier = savedInstanceState.getString(TAB_SELECTED);
@@ -73,11 +127,11 @@ public class MainActivity extends SherlockFragmentActivity implements com.action
 			selectTab(tabIdentifier, ft);
 		}
 	}
-	
+
 	// Image Cache
 	///////////////
-	
-	// Holy shit. This has to be a mistake... Fuck. 
+
+	// Holy shit. This has to be a mistake... Fuck.
 	private void setupImageFetcher() {
 		ImageFetcher imageFetcher = TIHUtils.newImageFetcher(this, 0.15f, getSupportFragmentManager());
 		EventListAdapter.imageFetcher = imageFetcher;
@@ -85,14 +139,14 @@ public class MainActivity extends SherlockFragmentActivity implements com.action
 		PhotoPitListAdapter.iconFetcher = TIHUtils.newImageFetcher(this, 0.05f, getSupportFragmentManager());
 		PhotoPitListAdapter.instaFetcher = TIHUtils.newImageFetcher(this, 0.15f, getSupportFragmentManager());
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getSupportMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		String urlString;
@@ -130,22 +184,22 @@ public class MainActivity extends SherlockFragmentActivity implements com.action
 		}
 		return(super.onOptionsItemSelected(item));
 	}
-	
+
 	private void launchWebViewWithUrl(String url) {
 		Intent webViewIntent = new Intent(this, TIHWebViewActivity.class);
 		webViewIntent.putExtra(TIHWebViewActivity.WEB_VIEW_URL, url);
 		startActivity(webViewIntent);
 	}
-	
+
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		//logger.d("onSaveInstanceState -  setting currentlySelectedTab: ", currentlySelectedTab);
 		outState.putString(TAB_SELECTED, currentlySelectedTab);
 	}
-	
+
 	private void detachFragment(FragmentTransaction ft) {
 		//logger.d("Detaching Fragment:", currentlySelectedTab);
-		if (currentlySelectedTab == null) { return; }; 
+		if (currentlySelectedTab == null) { return; };
 		if (currentlySelectedTab.equals(NEWS_TAB)) {
 			ft.detach(newsFragment);
 		} else if (currentlySelectedTab.equals(PHOTO_PIT_TAB)) {
@@ -156,7 +210,7 @@ public class MainActivity extends SherlockFragmentActivity implements com.action
 			//logger.d("Currently selected tab was null, nothing to detach");
 		}
 	}
-	
+
 	private void selectTab(String tabIdentifier, final FragmentTransaction ft) {
 		detachFragment(ft);
 		if (tabIdentifier.equals(NEWS_TAB)) {
@@ -167,7 +221,7 @@ public class MainActivity extends SherlockFragmentActivity implements com.action
 				newsFragment = NewsFragment.newInstance();
 				ft.add(R.id.fragment_container, newsFragment, NEWS_TAB);
 			} else {
-				ft.attach(newsFragment);	
+				ft.attach(newsFragment);
 			}
 		} else if (tabIdentifier.equals(SCHEDULE_TAB)) {
 			//logger.d("Schedule Tab Selected");
@@ -177,7 +231,7 @@ public class MainActivity extends SherlockFragmentActivity implements com.action
 				scheduleFragment = ScheduleFragment.newInstance();
 				ft.add(R.id.fragment_container, scheduleFragment, SCHEDULE_TAB);
 			} else {
-				ft.attach(scheduleFragment);	
+				ft.attach(scheduleFragment);
 			}
 		} else if (tabIdentifier.equals(PHOTO_PIT_TAB)) {
 			//logger.d("Photo Pit Tab Selected");
@@ -187,7 +241,7 @@ public class MainActivity extends SherlockFragmentActivity implements com.action
 				photoPitFragment = PhotoPitFragment.newInstance();
 				ft.add(R.id.fragment_container, photoPitFragment, PHOTO_PIT_TAB);
 			} else {
-				ft.attach(photoPitFragment);	
+				ft.attach(photoPitFragment);
 			}
 		} else {
 			//logger.d("onTabSelected -> couldn't find the tab with tag: ", tabIdentifier);
